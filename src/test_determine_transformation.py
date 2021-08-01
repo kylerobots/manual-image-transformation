5dcb3cb8b99e8e45f64d07539a49f971a3a1e9b4
Kyle M. Hart <kylemhart@gmail.com>
2021-08-01 16:31:20 -0400

    Add unit tests for file load.

---
+++ src/test_determine_transformation.py
@@ -0,0 +1,33 @@
+from PIL import UnidentifiedImageError
+from numpy import ndarray
+import determine_transformation
+import unittest
+
+
+class TestFileInput(unittest.TestCase):
+    """!
+    @brief Test the ability to read image files.
+    """
+
+    def test_good_file(self):
+        """!
+        @test Test that the function can read in valid image files.
+        """
+        result = determine_transformation.loadImage('data/000000.png')
+        self.assertIsInstance(
+            result, ndarray, 'Returned image is not a numpy array.')
+
+    def test_bad_file(self):
+        """!
+        @test Test that the function throws correctly when unable to open a file.
+        """
+        # Load some other non-image file.
+        self.assertRaises(UnidentifiedImageError,
+                          determine_transformation.loadImage, 'data/000000.txt')
+
+    def test_no_file(self):
+        """!
+        @test Test that the function throws correctly when a nonexistant file is specified.
+        """
+        self.assertRaises(
+            FileNotFoundError, determine_transformation.loadImage, 'data/999999.png')
