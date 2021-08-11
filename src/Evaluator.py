4ed60fee21d61111f800ac18d7dffeeaa34304ac
Kyle M. Hart <kylemhart@gmail.com>
2021-08-10 20:27:43 -0400

    Rename arguments to be consistent with other methods.

--- src/Evaluator.py
+++ src/Evaluator.py
@@ -92,7 +92,7 @@ class Evaluator:
         transformation[0:3, 3:] = translations[0]
         return transformation
 
-    def _findCorrespondence(self, keypoints1: list[cv2.KeyPoint], descriptors1: numpy.ndarray, keypoints2: list[cv2.KeyPoint], descriptors2: numpy.ndarray) -> tuple[numpy.ndarray, numpy.ndarray]:
+    def _findCorrespondence(self, first_keypoints: list[cv2.KeyPoint], first_descriptors: numpy.ndarray, second_keypoints: list[cv2.KeyPoint], second_descriptors: numpy.ndarray) -> tuple[numpy.ndarray, numpy.ndarray]:
         """!
         @brief Use descriptors to find matching keypoints in two sets.
 
@@ -106,10 +106,10 @@ class Evaluator:
         the two most similar keypoints in the second group, X and Y. A and X are considered a match if the distance
         between X and A is less than 70% of the distance between A and Y.
 
-        @param keypoints1 A list of keypoints from the first image.
-        @param descriptors1 The array containing the descriptors for these keypoints.
-        @param keypoints2 A list of keypoints from the second image.
-        @param descriptors2 The array containing the descriptors for these keypoints.
+        @param first_keypoints A list of keypoints from the first image.
+        @param first_descriptors The array containing the descriptors for these keypoints.
+        @param second_keypoints A list of keypoints from the second image.
+        @param second_descriptors The array containing the descriptors for these keypoints.
         @return tuple[numpy.ndarray, numpy.ndarray] Returns two matching arrays, each Nx2 where N are the number of
         matched keypoints. Each element[i, :] measures the pixel value of the keypoint in one or the other array.
         """
@@ -118,7 +118,7 @@ class Evaluator:
             cv2.DescriptorMatcher_FLANNBASED)
         # Find the candidate matches
         knn_matches = matcher.knnMatch(
-            queryDescriptors=descriptors1, trainDescriptors=descriptors2, k=2)
+            queryDescriptors=first_descriptors, trainDescriptors=second_descriptors, k=2)
         # Find which are confirmed matches
         good_matches = []
         for match_set in knn_matches:
@@ -132,73 +132,10 @@ class Evaluator:
         for i, match in enumerate(good_matches):
             # The match objects only provide which index in each keypoint list is a match. Use these to look up the
             # position information for the given keypoint.
-            matched_keypoint1 = keypoints1[match.queryIdx]
-            matched_keypoint2 = keypoints2[match.trainIdx]
+            matched_keypoint1 = first_keypoints[match.queryIdx]
+            matched_keypoint2 = second_keypoints[match.trainIdx]
             first_points[i][0] = matched_keypoint1.pt[0]
             first_points[i][1] = matched_keypoint1.pt[1]
             second_points[i][0] = matched_keypoint2.pt[0]
             second_points[i][1] = matched_keypoint2.pt[1]
         return (first_points, second_points)
-
-    # def calculateTransform(self, image1: numpy.ndarray, image2: numpy.ndarray) -> numpy.ndarray:
-    #     """!
-    #     @brief Estimate the transform a camera experienced when moving between the two images.
-
-    #     This method uses key points to find the transformation between images. It uses several helper functions and
-    #     classes to find matching key points in each image, then computes a likely transform from them.
-
-    #     @param image1 The image taken at the start of the transformation.
-    #     @param image2 The image taken at the end of the transformation.
-    #     @return numpy.ndarray A 3x3 numpy array representing the homogenous matrix for the 2D transformation.
-    #     """
-    #     detector = PointDetector(500)
-    #     # self.keypoints1 = detector.detect(image1)
-    #     # self.keypoints2 = detector.detect(image2)
-    #     self._tempDetect(image1, image2)
-    #     self._tempDescribe(image1, image2)
-    #     result = self._matchAndCalculate()
-    #     return result
-
-    # def _createKeypoints(self, keypoint_array: numpy.ndarray) -> cv2.KeyPoint:
-    #     """!
-    #     @brief A helper function to create OpenCV keypoints from
-    #     """
-
-    # def _matchAndCalculate(self) -> numpy.ndarray:
-    #     """!
-    #     @brief Match keypoints between images and use those to determine the transformation between images.
-
-    #     This method performs the calculations on the backend to compute the transformation between images. It leverages
-    #     OpenCV for convenience. It first finds related keypoints between images using a FLANN based KDTree search. It
-    #     then computes a viable transformation between images using those correspondences. This portion of the code is
-    #     taken from https://docs.opencv.org/3.4/d1/de0/tutorial_py_feature_homography.html.
-
-    #     @return numpy.ndarray Returns a 3x3 matrix representing a homogenous 2D tranformation matrix.
-    #     """
-    #     index_params = dict(algorithm=1, trees=5)
-    #     search_params = dict(checks=50)
-    #     flann = cv2.FlannBasedMatcher(index_params, search_params)
-    #     matches = flann.knnMatch(self.descriptions1, self.descriptions2, k=2)
-    #     good = []
-    #     for m, n in matches:
-    #         if m.distance < 0.7*n.distance:
-    #             good.append(m)
-    #     src_pts = numpy.float32(
-    #         [self.keypoints1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-    #     dst_pts = numpy.float32(
-    #         [self.keypoints2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-    #     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-    #     print(M.shape)
-    #     print(M)
-
-    #     return numpy.identity(3)
-
-    # def _tempDetect(self, image1, image2):
-    #     sift = cv2.SIFT_create()
-    #     self.keypoints1 = sift.detect(image1)
-    #     self.keypoints2 = sift.detect(image2)
-
-    # def _tempDescribe(self, image1, image2):
-    #     sift = cv2.SIFT_create()
-    #     (_, self.descriptions1) = sift.compute(image1, self.keypoints1)
-    #     (_, self.descriptions2) = sift.compute(image2, self.keypoints2)
